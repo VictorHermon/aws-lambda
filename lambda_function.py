80051ec59cf470ebd7fa4ed53754bbe382ba9326
Victor Hermon <victorsilva.silva6@gmail.com>
2025-03-10 00:13:40 -0300

    novo log

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ from logs import log
 def lambda_handler(event, context):
     # TODO implement
     log('Inicio de registro de evento')
+    log('Pra ver se tá indo o github actions')
     print(event)
     return {
         'statusCode': 200,
