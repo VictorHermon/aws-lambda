ba91f50dc57be943aaa5160a42e1781a0a33849f
Victor Hermon <victorsilva.silva6@gmail.com>
2025-03-10 00:04:48 -0300

    modificado mensagem de log

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ from logs import log
 
 def lambda_handler(event, context):
     # TODO implement
-    log('Inicio de registro de evento')
+    log('Inicio de registro de evento para validação')
     print(event)
     return {
         'statusCode': 200,
